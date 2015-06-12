af622849f6a06f3ad62f618cd2d22ff8afd6f613
sherwyn cooper <ballastvdotcom@gmail.com>
2015-06-11 20:03:02 -0400

    added first and last name cols to users table then added new fields to be permittable by model forms for both sign up and edit account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611033217) do
+ActiveRecord::Schema.define(version: 20150611232623) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 20150611033217) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
