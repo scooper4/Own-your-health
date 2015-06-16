808df79b67b441a4b4b2ff0e87fcff61e99773aa
sherwyn cooper <ballastvdotcom@gmail.com>
2015-06-16 10:39:36 -0400

    changed pagination to order by created_at desc

--- app/controllers/post_activities_controller.rb
+++ app/controllers/post_activities_controller.rb
@@ -7,7 +7,7 @@ before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote]
 
 	def index
      
-  		@post_activities = PostActivity.paginate(:page => params[:page])
+  		@post_activities = PostActivity.paginate(:page => params[:page] ).order("created_at desc")
       @users = User.order("total_points desc")
       @users.find_each do |user|
         user.update_total_points
