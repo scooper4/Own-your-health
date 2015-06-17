fe7311aeb7dd7df169909c5125d40f88671f9098
sherwyn cooper <ballastvdotcom@gmail.com>
2015-06-17 01:05:23 -0400

    can only view new workout on index page if signed in, can view comments without being signed in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:index]
 	before_action :find_comment, only: [ :edit, :update, :destroy]
 
   def index
