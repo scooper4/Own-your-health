e5ecfdff692c7df61e29fd839e4b1b587d5cd888
sherwyn cooper <ballastvdotcom@gmail.com>
2015-06-18 12:53:03 -0400

    update ruby gem file

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby'2.1.4'
+ruby'2.1.2'
 gem 'will_paginate', '~> 3.0.6'
 gem 'omniauth-facebook'
 gem 'acts_as_votable', '~> 0.10.0'
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module ErikHealth
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # fix heroku found on stack overflow
+    config.assets.initialize_on_precompile = false
   end
 end
