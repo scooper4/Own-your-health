299f3a4ef551da3aed7dd1ed077bebc522846116
sherwyn cooper <ballastvdotcom@gmail.com>
2015-06-23 07:05:30 -0400

    added (large) photo to fix image orientation problem

--- app/models/post_activity.rb
+++ app/models/post_activity.rb
@@ -1,10 +1,11 @@
 class PostActivity < ActiveRecord::Base
-	
+
   belongs_to :activity
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+  has_attached_file :image, :styles => { :large => "1000x1000", :medium => "300x300>", :thumb => "100x100>" }, 
   :default_url => "/images/:style/missing.png",
   :storage => :s3,
   :bucket  => ENV['S3_BUCKET_NAME'],
+   
   :access_key_id => ENV['AWS_ACCESS_KEY_ID']
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
@@ -16,12 +17,19 @@ class PostActivity < ActiveRecord::Base
   self.per_page = 5
 
 
+
+
+
   def post_points
   	activity.pts_per_min * act_mins
   end
+  
   def update_act_points
   	update(act_points: post_points.round(2))
   end
+
+  
+  
 
 
 end
